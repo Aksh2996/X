4a9382de248e89eaba8c030293e882d608f7073c
akshay <aprakash270@gmail.com>
2025-07-12 00:46:53 +0530

    commit

---
+++ WhileDemo.py
@@ -0,0 +1,22 @@
+it = 10
+
+while it>1:
+    if it == 9:
+        it = it - 1
+        continue
+    if it == 3:
+        break
+    print(it)
+    print("hello22")
+    print("ki")
+    print("Ki2")
+    print("Test1")
+
+
+
+    it = it - 1
+    print("Yes")
+
+print('while loop execution is done')
+
+
